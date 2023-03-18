aa3d8d145aa54739065481369bd261fc65b38cac
Manuel Sanchez Pinar <aomanu@gmail.com>
2023-03-18 12:38:20 +0100

    v2023031100 => v2023031800

--- commit-all.sh
+++ commit-all.sh
@@ -12,7 +12,7 @@ fi
 
 set -e
 git add -A
-git commit -S -m "$last_tag => $new_tag"
+git commit -S -m "$last_tag => $new_tag" || true
 git tag -m "$new_tag" -s "$new_tag"
 git push --all
 git push --tags
